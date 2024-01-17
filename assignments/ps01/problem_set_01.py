e54bde2d307639c53004539f238543fc9a4c91bd
NakaFrozn <jonaszhxie@outlook.com>
2024-01-16 23:15:22 -0500

    Create ps01

---
+++ assignments/ps01/problem_set_01.py
@@ -0,0 +1,89 @@
+# START PROBLEM SET 1
+print("Problem Set 1 \n")
+
+# PROBLEM 01
+print("\nPROBLEM 01")
+
+# 1frita_batidos = "Frita Batidos"
+# zingerman's Delicatessen = "Zingerman's Delicatessen"
+# nypd = New York Pizza Depot
+# hopcat = "HoPcAT"
+# fleetwood_diner = "Fleetwood Diner"
+# tomukun_noodle_bar = "Tomukun Noodle Bar
+# jerk_pit = "JaMAIcaN JERk PIt"
+# mama satto = "Mama Satto"
+# hola_seoul = "Hola Seoul"
+# @shalimar = "Shalimar"
+
+cottage_inn = None
+print(cottage_inn)
+
+madras_masala = None
+print(madras_masala)
+
+# PROBLEM 02
+print("\nPROBLEM 02")
+
+hopcat_all_lower = None
+print(f"all_lower: {hopcat_all_lower}")
+
+jerk_pit_all_upper = None
+print(f"all_upper: {jerk_pit_all_upper}")
+
+madras_masala_count_m = None
+print(f"number of letter m: {madras_masala_count_m}")
+
+has_diner = None
+print(has_diner)
+
+starts_seoul = None
+print(starts_seoul)
+
+comment = "Great Jamaican food&drinks"
+updated_comment = None
+print(f"updated comment: {updated_comment}")
+
+# PROBLEM 03
+print("\nPROBLEM 03")
+
+num_chars = None
+print(f"number of characters: {num_chars}")
+
+restaurants = [
+    "Frita Batidos",
+    "Zingerman's Delicatessen",
+    "New York Pizza Depot",
+    "Hopcat",
+    "Fleetwood Diner",
+    "Tomukun Noodle Bar",
+    "Jamaican Jerk Pit",
+    "Mama Satto",
+    "Hola Seoul",
+    "Shalimar",
+    "Cottage Inn Pizza",
+    "Madras Masala",
+]
+
+print(None)
+
+num_restaurants = None
+print(f"number of restaurants: {num_restaurants}")
+
+# PROBLEM 04
+print("\nPROBLEM 04")
+total_price = None
+print(f"total price: {total_price}")
+
+total_bill = None
+print(f"total bill: {total_bill}")
+
+each_pay = None
+
+print(f"each person pays: {each_pay}")
+
+# PROBLEM 05
+print("\nPROBLEM 05")
+
+# TODO: Create f-string
+
+# END PROBLEM SET
